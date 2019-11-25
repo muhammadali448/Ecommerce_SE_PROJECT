77a102d37721dcde9f22aca4b8799c2d233984ee
John Wick <muhammadali@cqtechnologies.com>
2019-11-25 10:27:41 +0500

    category api done

--- controllers/category.js
+++ controllers/category.js
@@ -1,10 +1,55 @@
 const Category = require("../models/category");
-
 exports.create = async (req, res) => {
   try {
     const category = new Category(req.body);
     const data = await category.save();
-    res.json({ data });
+    res.json(data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.getCategory = async (req, res) => {
+  res.json(req.category);
+};
+
+exports.deleteCategory = async (req, res) => {
+  try {
+    await Category.findByIdAndDelete(req.category.id);
+    res.json({ message: "Successfully Deleted" });
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.getList = async (req, res) => {
+  try {
+    const data = await Category.find().exec();
+    res.json(data);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.updateCategory = async (req, res) => {
+  try {
+    const category = req.category;
+    category.name = req.body.name;
+    const updatedCategory = await category.save();
+    res.json(updatedCategory);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.findCategoryById = async (req, res, next, id) => {
+  try {
+    const category = await Category.findById(id).exec();
+    if (!category) {
+      return res.status(404).json({ error: "Category not exist" });
+    }
+    req.category = category;
+    next();
   } catch (error) {
     res.json({ error: error.message });
   }
--- routes/category.js
+++ routes/category.js
@@ -2,7 +2,14 @@ const express = require("express");
 const { requireSigin, isAuth, isAdmin } = require("../controllers/auth");
 const { findUserById } = require("../controllers/user");
 const { createValidation } = require("../validation/category");
-const { create } = require("../controllers/category");
+const {
+  create,
+  findCategoryById,
+  getCategory,
+  deleteCategory,
+  updateCategory,
+  getList
+} = require("../controllers/category");
 const router = express.Router();
 
 router.post(
@@ -13,5 +20,23 @@ router.post(
   createValidation,
   create
 );
+router.delete(
+  "/delete/:userId/:categoryId",
+  requireSigin,
+  isAuth,
+  isAdmin,
+  deleteCategory
+);
+router.put(
+  "/update/:userId/:categoryId",
+  requireSigin,
+  isAuth,
+  isAdmin,
+  createValidation,
+  updateCategory
+);
+router.get("/get/:categoryId", getCategory);
+router.get("/list", getList);
+router.param("categoryId", findCategoryById);
 router.param("userId", findUserById);
 module.exports = router;
