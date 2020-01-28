f98ccd797cc2d3bd1b26b17bfdf75848c60c63ba
cq-m-ali <muhammad.ali@cqtechnologies.com>
2020-01-28 21:21:48 +0500

    orders oky---

--- controllers/order.js
+++ controllers/order.js
@@ -11,6 +11,46 @@ exports.create = async (req, res) => {
   }
 };
 
+exports.findOrderById = async (req, res, next, id) => {
+  try {
+    const order = await Order.findById(id);
+    if (!order) {
+      res.status(404).json({ error: "Order not found" });
+    }
+    req.order = order;
+    next();
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.getStatusValues = async (req, res) => {
+  try {
+    const statusValues = Order.schema.path("status").enumValues;
+    res.json(statusValues);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
+exports.updateStatusValue = async (req, res) => {
+  try {
+    const updateOrder = await Order.findByIdAndUpdate(
+      req.params.orderId,
+      {
+        $set: { status: req.body.status }
+      },
+      { new: true }
+    );
+    if (!updateOrder) {
+      return res.status(400).json({ error: "Orders not updated" });
+    }
+    res.json(updateOrder);
+  } catch (error) {
+    res.json({ error: error.message });
+  }
+};
+
 exports.list = async (req, res) => {
   try {
     const orders = await Order.find()
--- routes/order.js
+++ routes/order.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const { requireSigin, isAuth, isAdmin } = require("../controllers/auth");
 const { findUserById, addOrderToUserHistory } = require("../controllers/user");
-const { create, list } = require("../controllers/order");
+const {
+  create,
+  list,
+  getStatusValues,
+  updateStatusValue,
+  findOrderById
+} = require("../controllers/order");
 const { decreaseQuantityandIncreaseSold } = require("../controllers/product");
 const router = express.Router();
 
 router.get("/list/:userId", requireSigin, isAuth, isAdmin, list);
+router.get(
+  "/list/statusValues/:userId",
+  requireSigin,
+  isAuth,
+  isAdmin,
+  getStatusValues
+);
+
+router.put(
+  "/statusValue/:userId/update/:orderId",
+  requireSigin,
+  isAuth,
+  isAdmin,
+  updateStatusValue
+);
 
 router.post(
   "/create/:userId",
@@ -17,4 +38,5 @@ router.post(
 );
 
 router.param("userId", findUserById);
+router.param("orderId", findOrderById);
 module.exports = router;
